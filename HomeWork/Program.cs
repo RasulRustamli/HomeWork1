ee84350082c415d3f0c22f62d73fc3ce6e33d49f
RasulRustamli <rasular@code.edu.az>
2021-10-03 18:41:43 +0400

    Proje dosyası ekle.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            // 3 ve 7 ye bolunme
+
+            //int n = 21;
+            //if (n % 21 == 0)
+            //{
+            //    Console.WriteLine("eded 3-e ve 7 ye bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 3-e ve 7ye bolunmur");
+            //}
+            #endregion
+
+            #region
+            // qardrobun kapidan kecib kecmemesi
+
+
+            //int a = 10; //door width
+            //int b = 15; //door height
+            //int c = 10; //wardrobe width
+            //int d = 11; //wardrobe height
+            //int f = 9; //thickess of wardrobe
+            //if((c<=a && d <= b) || (f<=a && d<=b) ||(c<=b && f<=a)||(c<=a &&f<=b))
+            //{
+            //    Console.WriteLine("wardrobe passes through the door");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("wardrobe does not pass though the door");
+            //}
+            #endregion
+
+            #region
+            // 2 eded arasindaki tek ededlerin sayi
+
+            //int n = 10;
+            //int m = 25;
+            //int f = 0;
+            //for (int i = n;i<=m; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        f++;
+            //    }
+
+            //}
+            //Console.WriteLine(f);
+            #endregion
+
+            #region
+            //verilmis n ededin sade ve ya murekkeb olmagini mueyyen etmek
+            //int n = 30;
+            //int sayi = 0;
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        sayi++;
+            //    }
+
+            //}
+            //if (sayi > 2)
+            //{
+            //    Console.WriteLine("verilmis eded murekkebdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("verilmis eded sadedir");
+            //}
+            #endregion
+            #region
+            //ededin nece mertebeli oldugunu mueyyen etmek
+            //int n = 20;
+            //int sayi = 0;
+            //if (n != 0)
+            //{
+            //    while (n >= 1)
+            //    {
+            //        n /= 10;
+            //        sayi++;
+            //    }
+            //    Console.WriteLine(sayi);
+            //}
+            //Console.WriteLine("duzgun eded daxil edin");
+            #endregion
+
+            #region
+            //EDEDIN 2 NIN QUVVETI OLUB OLMADIGINI MUEYYEN ETMEK
+            //int n = 33;//verilmis eded
+            //while (n % 2 == 0)
+            //{
+            //    n/=2;
+            //}
+            //if (n == 1)
+            //{
+            //    Console.WriteLine("quvvetidir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("quvveti deyil");
+            //}
+            #endregion
+
+        }
+    }
+}
